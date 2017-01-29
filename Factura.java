995a316d816d2cf6d530e0e1f65da30e26e750a2
MemoBG <barrientos.memo@gmail.com>
2017-01-28 19:37:30 -0600

    Jajajaja

--- Factura.java
+++ Factura.java
@@ -10,22 +10,12 @@ public class Factura {
 	description, invoiceDate, accontingDate, businessPartner, businessPartnerIdentifier, 
 	partnerAddress, orderDate, currency, paymentTerms, priceList, salesCampaign, paymentMethod,
 	feTimbreDate, feTimbreUuid, grandTotalAmount;
-<<<<<<< HEAD
 
 String id, client, clientIdentifier, organization, creationDate, createdByIdentifier, 
 	documentNo, documentStatus, documentAction, documentType, transactionDocument, salesOrder, 
 	description, invoiceDate, accontingDate, businessPartner, businessPartnerIdentifier, 
 	partnerAddress, orderDate, currency, paymentTerms, priceList, salesCampaign, paymentMethod,
 	feTimbreDate, feTimbreUuid, grandTotalAmount;
-
-
-String id, client, clientIdentifier, organization, creationDate, createdByIdentifier, 
-	documentNo, documentStatus, documentAction, documentType, transactionDocument, salesOrder, 
-	description, invoiceDate, accontingDate, businessPartner, businessPartnerIdentifier, 
-	partnerAddress, orderDate, currency, paymentTerms, priceList, salesCampaign, paymentMethod,
-	feTimbreDate, feTimbreUuid, grandTotalAmount;
-=======
->>>>>>> 1c8c8600b7954eaa881e239e6c82f9c2724f955f
 
 	
 
